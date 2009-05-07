499e61dc54f71b3b918b6b94d9a73f31a08d9430
Eric Redmond <eric.redmond@gmail.com>
2009-05-07 16:01:15 -0400

    added RSS capability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
+# Copyright 2009 Eric Redmond
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,16 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-
-
 import os
 import wsgiref.handlers
 
-
 from google.appengine.ext.webapp import template
-from google.appengine.ext import webapp
-from google.appengine.ext import db
+from google.appengine.ext import webapp, db
+
+BASE_URL = 'http://www.textbarf.com/'
 
 class Barf(db.Model):
   text = db.StringProperty(required=True)
@@ -37,6 +34,18 @@ class MainHandler(webapp.RequestHandler):
     
     path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
     self.response.out.write(template.render(path, {'barfs': barfs}))
+
+
+class FeedHandler(webapp.RequestHandler):
+  def get(self):
+    barfs = Barf.all().order('-date').fetch(250)
+    
+    self.response.headers['Content-Type'] = 'application/rss+xml'
+    self.response.out.write('<?xml version="1.0"?><rss version="2.0"><channel>')
+    self.response.out.write("<title>Text Barf Feed</title><link>%s</link><description></description>" % BASE_URL)
+    for b in barfs:
+      self.response.out.write("<item><title>%s</title><link>%s</link><description>%s</description></item>" % (b.text, BASE_URL, b.text))
+    self.response.out.write("</channel></rss>")
 
 
 class TxtBackHandler(webapp.RequestHandler):
@@ -58,7 +67,8 @@ class TxtBackHandler(webapp.RequestHandler):
 
 def main():
   application = webapp.WSGIApplication([('/', MainHandler),
-                                        ('/txtback', TxtBackHandler)],
+                                        ('/txtback', TxtBackHandler),
+                                        ('/rss.xml', FeedHandler)],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
